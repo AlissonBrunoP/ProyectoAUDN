5e44b7ba5677e29a8c071fa6b93e4f9627177d70
Alisson Bruno <alissond.brunopadron@gmail.com>
2023-05-15 22:34:32 -0300

    mejoras en css

--- frontend/src/Components/Lobby/Lobby.jsx
+++ frontend/src/Components/Lobby/Lobby.jsx
@@ -2,7 +2,8 @@ import React from "react";
 import "./Lobby.css";
 import { NavLink, useNavigate } from "react-router-dom";
 import logotipo from "../../assets/icons/logo-large.svg"
-
+import logoApple from "../../assets/icons/apple-logo.svg"
+import logoGoogle from "../../assets/icons/google-logo.svg"
 function Lobby() {
   const navigate =useNavigate();
 
@@ -17,20 +18,22 @@ function Lobby() {
           <img src={logotipo} alt="AUDN" className="logotipo" />
         </div>
 
-        <h3 className="titleLobby">Música a medida</h3>
+        <h3 className="titleLobby">Música a medida.</h3>
         <section className="sectionLobby">
           <button type="button" className="registrarsegratis" onClick={goToRegister}>
             Registrarse Gratis
           </button>
           <button type="button" className="continuarcongoogle">
+           <img src={logoGoogle} alt="" />
             Continuar con Google
           </button>
           <button type="button" className="continuarconapple">
+           <img src={logoApple} alt="" />
             Continuar con Apple
           </button>
           <div className="containerbutton">
             <button type="button" className="iniciarsesion">
-              <NavLink to="/login"> Iniciar sesión </NavLink>
+              <NavLink to="/login" id="navLogin"> Iniciar sesión </NavLink>
             </button>
           </div>
         </section>
